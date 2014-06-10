9dce1762118ae1183a17f4157a7016fec1200358
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-06-10 13:20:40 -0400

    Project updated

--- scripts/epistasis.sh
+++ scripts/epistasis.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 
-numBases="3"
-distance="3"
+distance="5.0"
 
-# # Perform PDB distance analysis
-#java -jar Epistasis.jar pdbdist 8 db/pdb
-java -jar Epistasis.jar pdbdist $distance db/pdb db/pfam/ensemblId_to_proteinId.txt  > pdbdist.$distance.txt
-mv pdb_distance_by_AA_pos.txt pdb_distance_by_AA_pos.$distance.txt
-
-# # Mutual information analysis
-# java -jar Epistasis.jar \
-# 	mi \
-# 	100 \
-# 	$numBases \
-# 	/home/pcingola/snpEff/db/multiz100way/head.fa \
-# 	2> epistasis.mi.$numBases.err \
-# 	> epistasis.mi.$numBases.out
+java -Xmx4G -jar Epistasis.jar \
+		checkPdbGenome \
+		/home/pcingola/snpEff/snpEff.config  \
+		hg19 \
+		/home/pcingola/snpEff/db/pdb/pdb_hires_human/ \
+		/home/pcingola/snpEff/db/multiz100way/hg19.100way.nh \
+		/home/pcingola/snpEff/db/multiz100way/head.fa \
+		/home/pcingola/snpEff/db/multiz100way/idMap_ensemblId_refseq_pdbId.txt \
+		5.0 \
+	| tee epistasis.out
 
