8a67c39854970045a9244686e7f3da036ef76cc5
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-07-15 11:17:11 -0400

    Project updated

--- scripts/showLikelihoods.pl
+++ scripts/showLikelihoods.pl
@@ -2,6 +2,7 @@
 
 while( $l = <STDIN> ) {
 	chomp $l;
-	($id1, $id2, $ll, $ln, $la, $s1, $s2) = split /\t/, $l;
+	#($id1, $id2, $ll, $ln, $la, $s1, $s2) = split /\t/, $l;
+	($ll, $ln, $la, $s1, $s2) = split /\t/, $l;
 	print "$ll\t$ln\t$la\n\t$s1\n\t$s2\n\n";
 }
