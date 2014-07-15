f1b4b2b41fe22481c89c753ae321eb9012714cb3
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-07-15 10:01:16 -0400

    Project updated

--- scripts/loglikelihood.r
+++ scripts/loglikelihood.r
@@ -8,33 +8,36 @@ if( savePlot ) { png( width=800, height=800 ) }
 
 if( ! exists('lc') ) {
 	lc <- read.csv("likelihood.contact.values.txt")
-	la <- read.csv("likelihood.all.values.txt")
+	la <- read.csv("likelihood.null.values.txt")
 
 	lc <- as.numeric( lc[,1] )
 	la <- as.numeric( la[,1] )
 
 }
 
-xmin <- -400
-xmax <- 50
+xmin <- -40
+xmax <- 25
 xlim <- c(xmin, xmax)
-breaks <- 100
+breaks <- seq(xmin, xmax, 1)
 overlap <- F
 
 # In contact
 data <- lc[ (xmin <= lc) & (lc < xmax) ]
 dens <- density(data)
-h <- hist(data, main="Log likelyhood ratio histogram", xlim=xlim, xlab = "data", ylab = "Frequency", freq = F, breaks=breaks, col=rgb(1,0,0,1/4) , add=F );
+hc <- hist(data, main="Log likelyhood ratio histogram", xlim=xlim, xlab = "data", ylab = "Frequency", freq = F, breaks=breaks, col=rgb(1,0,0,1/4) , add=F );
 lines(dens, col='red', xlim=xlim)
 
 # All
 data <- la[ (xmin <= la) & (la < xmax) ]
 dens <- density(data)
-h <- hist(data, xlim=xlim, xlab = "data", ylab = "Frequency", freq = F, breaks=breaks, col=rgb(0,0,1,1/4), add=T );
+ha <- hist(data, xlim=xlim, xlab = "data", ylab = "Frequency", freq = F, breaks=breaks, col=rgb(0,0,1,1/4), add=T );
 lines(dens, col='blue', xlim=xlim)
 
-
 legend("topleft",c("In contact","All"),lty=c(1,1),col=c("red","blue"))
+
+dens.ratio <- hc$density / ha$density
+plot( hc$mids, dens.ratio, , main='Log likelyhood density ratio', xlab='Log likelihood', ylab='Density Ratio')
+lines( hc$mids, dens.ratio )
 
 if( savePlot )  { dev.off() }
 
