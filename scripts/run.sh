f60ff8c3f863779dfc97f4ae71654f22b7fce32e
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-08-28 22:02:30 -0400

    Project updated

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 echo Rmoving old files
-rm nohup.out
-rm -rvf epistasis.bds.* run.bds.* 
+rm -rvf nohup.out epistasis.bds.* run.bds.* 
 
 echo Process is run as nohup
 nohup `dirname $0`/run.bds &
