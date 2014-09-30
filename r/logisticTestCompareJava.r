2fee428b596f6418063d6d3fc002f4600942a513
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-09-30 16:35:29 -0400

    Project updated

---
+++ r/logisticTestCompareJava.r
@@ -0,0 +1,32 @@
+
+#-------------------------------------------------------------------------------
+#
+# Logistic regression and likelihood ratio test
+# Used for debugging data from LogisticeRegression class (Java code)
+#
+#																Pablo Cingolani
+#-------------------------------------------------------------------------------
+
+library(epicalc)
+
+# Load data from TXT file. See Regression.toStringSample() method
+d <- read.csv("lr_test.alt.txt", header = TRUE, sep="\t")
+
+#---
+# Calculate logistic regression modelis
+#---
+
+for( count in 1:100 ) {
+	# Full model, takes into account genotypes and PCs
+	lr.alt  <- glm( out ~ in0 + in1 + in2 + in3 + in4 + in5 + in6 + in7 + in8 + in9 + in10 + in11 + in12 , family=binomial, data=d)
+
+	# Reduced model: only covariates, no genotypes
+	lr.null <- glm( out ~       in1 + in2 + in3 + in4 + in5 + in6 + in7 + in8 + in9 + in10 + in11 + in12 , family=binomial, data=d)
+
+	# Likelyhood ratio test
+	lik.ratio.test <- lrtest(lr.alt, lr.null)
+	pvalue <- lik.ratio.test$p.value   # p-value from likelihood ration test
+
+	cat('Iteration', count, '\n')
+}
+
