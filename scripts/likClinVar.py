e7e820c00d296d2619175a13d6791c729c90714e
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-11-06 17:20:41 -0500

    Project updated

--- scripts/likClinVar.py
+++ scripts/likClinVar.py
@@ -15,18 +15,32 @@ def msaIdParse(id):
 linesByvcfId = dict()
 
 # Read stdin
-print "Reading stdin:"
 count = 1
+llrsame = 0.0
+llrmax = 0.0
+msaId = ''
 for l in sys.stdin:
 	# Parse input
 	(vcfId, msa1, msa2, llr, llnull, llalt) = l.rstrip().split('\t')
 	(trid1, start1, end1, aaidx1) = msaIdParse(msa1)
 	(trid2, start2, end2, aaidx2) = msaIdParse(msa2)
 
-	if  msa1 == msa2: llr = "-1"
-	elif float(llr) < 0: llr = "0"
+	ll = float(llr)
+
+	if  msa1 == msa2: 
+		llrsame = ll
+		llr = "-1"
+		msaId = msa1
+	elif ll < 0: 
+		llr = "0"
+	else:
+		llrmax = max(llrmax, ll)
+		
 
 	# Add to map
 	print "\t".join( (trid1, start1, end1, aaidx1, trid2, start2, end2, aaidx2, llr, llnull, llalt) )
 
 	count += 1
+
+
+print >> sys.stderr, msaId + '\t' + str(llrmax) + '\t' + str(llrsame)
