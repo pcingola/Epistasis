9de8cce47da75c0f3a435099c7efb754467d4c58
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-11-11 18:30:44 -0500

    Project updated

---
+++ scripts/likelihood_add_CLNSIG.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+$debug = 0;
+
+#---
+# Read clinvar
+#---
+open CLINVAR, "../clinvar-latest.eff.vcf" or die "Cannot open file\n";
+while( $l = <CLINVAR> ) {
+	chomp $l;
+	($chr, $pos, $id, $ref, $alt, $q, $filter, $info) = split /\t/, $l;
+
+	if( $info =~/CLNSIG=(.*?);/ ) {
+		$clnsig = $1;
+		if( $clnsig =~ /(\d+)[,|].*/ )	{ 
+			print "$clnsig\t=>\t$1\n" if $debug;
+			$clnsig = $1; 
+		}
+
+		$clnsig{"$chr:$pos"} = $clnsig;
+		print "$chr\t$pos\t$clnsig\n" if $debug;
+	}
+}
+close CLINVAR;
+
+# Read STDIN
+print "chr\tpos\tref\talt\tll\tclnsig\n";
+while( $l = <STDIN> ) {
+	chomp $l;
+	($chr, $pos, $ref, $alt, $ll) = split /\t/, $l;
+
+	$pos += 1;
+	print "$chr\t$pos\t$ref\t$alt\t$ll\t" . $clnsig{"$chr:$pos"} . "\n";
+}
