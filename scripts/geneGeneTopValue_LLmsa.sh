0d8ecb28a1016770081171c9bd096af910252fcb
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-11-18 09:15:40 -0500

    Project updated

--- scripts/geneGeneTopValue_LLmsa.sh
+++ scripts/geneGeneTopValue_LLmsa.sh
@@ -3,5 +3,5 @@
 for txt in `find . -iname "*.txt"`
 do
 	max=`cat $txt | ./geneGeneTopValue_LLmsa.pl`
-	echo $txt $max | tr "/ " "\t\t" | sed "s/.txt//" | cut -f 2- | tee -a geneGeneTopValue_LLmsa.txt
+	echo $txt $max | tr "/ " "\t\t" | sed "s/.txt//" | cut -f 2- | tee -a geneGeneTopValue_LLmsa_2.txt
 done
