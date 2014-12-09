68c112cbe0b4cc7c7bdc129fa096531551a9e1bd
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-12-09 12:41:33 -0500

    Project updated

--- scripts/pdbCompoundLines.sh
+++ scripts/pdbCompoundLines.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-for pdb in `find . -iname "*.pdb"`
+dir=$1
+
+for pdb in `find $dir -iname "*.pdb"`
 do
 	pdbId=`basename $pdb .pdb`
 	cat $pdb | $HOME/snpEff/epistasis/scripts/pdbCompoundLines.py $pdbId
