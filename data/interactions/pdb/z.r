6a1335daa03982707f0efb7882197a8b1fbabb15
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-12-11 17:35:23 -0500

    Project updated

--- data/interactions/pdb/z.r
+++ data/interactions/pdb/z.r
@@ -2,20 +2,36 @@
 savePng <- T
 
 if( savePng )	png(width=1024, height=1024)
-for( neigh in 1:4 ) {
+for( neigh in 1:1 ) {
 	cat('\nNeighbours:', neigh, '\n')
 	d.int <- read.table(paste('likelihood.pdb_compound.neigh_', neigh, '.3.0.txt', sep=''), header=F, sep='\t')
 	d.non <- read.table(paste('likelihood.pdb_compound.neigh_', neigh, '.-30.0.txt', sep=''), header=F, sep='\t')
 
 	ll.int <- d.int[,5]
 	ll.non <- d.non[,5]
-
-#	neigh <- 0
-#	ll.int <- d.int[,3]
-#	ll.non <- d.non[,3]
-
+	xlim <- c(-20,15)
+	title <- 'LL(MSA) of interacting proteins (PDB)'
+	xlab <- 'LL(MSA)'
 	sub <- paste('Neighbours:', 0) # neigh * 2 + 1)
-	plot( density( ll.int ), main='LL(MSA) of interacting proteins (PDB)', sub=sub, xlim= c(-20,15), xlab='LL(MSA)', col='red' )
+
+	# Use MI
+	ll.int <- d.int[,15]
+	ll.int <- ll.int[ ll.int > 0 ]
+	ll.non <- d.non[,15]
+	ll.non <- ll.non[ ll.non > 0 ]
+	xlim <- c(0, 1)
+	title <- 'Mutual information of interacting proteins (PDB)'
+	xlab <- 'MI'
+	sub <- ''
+
+#	# Use variation of information
+#	ll.int <- d.int[,16]
+#	ll.int <- ll.int[ ll.int > 0 ]
+#	ll.non <- d.non[,16]
+#	ll.non <- ll.non[ ll.non > 0 ]
+#	xlim <- c(0, 3.1)
+
+	plot( density( ll.int ), main=title, sub=sub, xlim=xlim, xlab=xlab, col='red' )
 	cat('Interacting\t', summary(ll.int), '\n')
 
 	lines( density( ll.non ), col='green' )
