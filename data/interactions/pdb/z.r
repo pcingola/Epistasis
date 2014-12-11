942b4eb35e3d062913ef5a64e247f242da99d301
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-12-11 14:08:04 -0500

    Project updated

--- data/interactions/pdb/z.r
+++ data/interactions/pdb/z.r
@@ -1,23 +1,25 @@
 
 savePng <- T
+
 if( savePng )	png(width=1024, height=1024)
+for( neigh in 1:4 ) {
+	cat('\nNeighbours:', neigh, '\n')
+	d.int <- read.table(paste('likelihood.pdb_compound.neigh_', neigh, '.3.0.txt', sep=''), header=F, sep='\t')
+	d.non <- read.table(paste('likelihood.pdb_compound.neigh_', neigh, '.-30.0.txt', sep=''), header=F, sep='\t')
 
-neigh <- 1
+	ll.int <- d.int[,5]
+	ll.non <- d.non[,5]
 
-d.int <- read.table(paste('likelihood.pdb_compound.neigh_', neigh, '.3.0.txt', sep=''), header=F, sep='\t')
-d.non <- read.table(paste('likelihood.pdb_compound.neigh_', neigh, '.-30.0.txt', sep=''), header=F, sep='\t')
+	sub <- paste('Neighbours:', neigh * 2 + 1)
+	plot( density( ll.int ), main='LL(MSA) of interacting proteins (PDB)', sub=sub, xlim= c(-20,15), xlab='LL(MSA)', col='red' )
+	cat('Interacting\t', summary(ll.int), '\n')
 
-ll.int <- d.int[,5]
-ll.non <- d.non[,5]
+	lines( density( ll.non ), col='green' )
+	cat('Non-interacting\t', summary(ll.non), '\n')
 
-sub <- paste('Neighbours:', neigh * 2 + 1)
-plot( density( ll.int ), main='LL(MSA) of interacting proteins (PDB)', sub=sub, xlim= c(-20,15), xlab='LL(MSA)', col='red' )
-cat( summary(ll.int), '\n' )
-
-lines( density( ll.non ), col='green' )
-cat( summary(ll.non), '\n' )
-
-legend("topleft", inset=.05, c('Interacting', 'Non-interacting'), fill=c('red','green'), horiz=F)
+	legend("topleft", inset=.05, c('Interacting', 'Non-interacting'), fill=c('red','green'), horiz=F)
+}
+if( savePng )	dev.off()
 
 
 # cols <- 1:dim(d)[2]
