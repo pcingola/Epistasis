78b823182b65d0650b635cd48c5f5c3692481514
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-02-28 21:06:56 -0500

    Project updated

--- th/make.sh
+++ th/make.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # Delete old 'tmp' files
-rm -vf *.bbl *.aux *.blg *.log *.lof *.lot *.pdf *.synctex.gz
+#rm -vf *.bbl *.aux *.blg *.log *.lof *.lot *.pdf *.synctex.gz
 
+#pdflatex thesis_mcgill.tex
+#bibtex thesis_mcgill
 pdflatex thesis_mcgill.tex
 bibtex thesis_mcgill
-pdflatex thesis_mcgill.tex
-bibtex thesis_mcgill
+open thesis_mcgill.pdf
+
