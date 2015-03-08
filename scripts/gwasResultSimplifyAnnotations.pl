4fe00b5b1a87641a1215268f76d8d76a8bf7f6d1
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-03-07 19:17:42 -0500

    Project updated

--- scripts/gwasResultSimplifyAnnotations.pl
+++ scripts/gwasResultSimplifyAnnotations.pl
@@ -9,15 +9,18 @@ use strict;
 sub annotate($) {
 	my($ann) = @_;
 
-	my($eff, $gene, $e, $g, @anns, %effs, %genes);
+	my($eff, $gene, $impact, $e, $g, @anns, %effs, %genes);
 	@anns = split /,/, $ann;
 	foreach $ann ( @anns ) {
 		# Format example
 		# 		NON_SYNONYMOUS_CODING(MODERATE|MISSENSE|cGc/cCc|R433P|714|CAPN1|protein_coding|CODING|ENST00000279247|11|1)
-		if( $ann =~ /(.+)\(.*?\|.*?\|.*?\|.*?\|.*?\|(.*?)\|.*/ ) {
-			($eff, $gene) = ($1, $2);
-			$effs{$eff} = 1;
-			$genes{$gene} = 1;
+		if( $ann =~ /(.+)\((.*?)\|.*?\|.*?\|.*?\|.*?\|(.*?)\|.*/ ) {
+			($eff, $impact, $gene) = ($1, $2, $3);
+
+			if(($impact eq 'MODERATE') || ($impact eq'HIGH')) {
+				$effs{$eff} = 1;
+				$genes{$gene} = 1;
+			}
 		}
 	}
 
@@ -79,33 +82,27 @@ sub shared($$) {
 # Main
 #-------------------------------------------------------------------------------
 
-# Parse command line options
-die "Usage: cat file.txt | gwasResultSimplifyAnnotations.pl {BF|MSA}" if $#ARGV < 0;
-
-my($BF) = 0;
-if( $ARGV[0] eq 'BF' )	{ $BF = 1; }
-elsif( $ARGV[0] eq 'MSA' )	{ $BF = 0; }
-else { die "Unknown command line option '$ARGV[0]'\n"; }
-
 # Parse input
-my($l, $ann1Str, $ann2Str, $share, $bf, $pva, $ll, $lllr, $llmsa, $id1, $id2, $ann1, $ann2);
+my($l, $ann1Str, $ann2Str, $share, $i);
+
+my @removeLabels = (3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ,11 ,12 ,17 ,18);
+
 while( $l = <STDIN> ) {
 	chomp $l;
+	my @t = split /\t/, $l;
 
-	if( $BF ) {
-		($bf, $pva, $ll, $lllr, $llmsa, $id1, $id2, $ann1, $ann2) = split /\t/, $l;
-	} else {
-		($pva, $ll, $lllr, $llmsa, $id1, $id2, $ann1, $ann2) = split /\t/, $l;
+	# Split semicolon and discard label + Remove spaces
+	foreach $i (@removeLabels) {
+		if( $t[$i] =~/(.+):(.+)/ ) {
+			$t[$i] = $2;
+			$t[$i] =~ tr/ //ds;
+		}
 	}
 
-	$ann1Str = annotate($ann1);
-	$ann2Str = annotate($ann2);
+	# Extract genes for MODERATE and HIGH impact variants
+	$t[15] = annotate($t[15]);
+	$t[16] = annotate($t[16]);
 
-	$share = shared($ann1Str, $ann2Str);
-
-	if( $BF ) {
-		print "$bf\t$pva\t$ll\t$lllr\t$llmsa\t$id1\t$id2\t$ann1Str\t$ann2Str\t$share\t$ann1\t$ann2\n";
-	} else {
-		print "$pva\t$ll\t$lllr\t$llmsa\t$id1\t$id2\t$ann1Str\t$ann2Str\t$share\t$ann1\t$ann2\n";
-	}
+	# Show results
+	print join("\t", @t) . "\n";
 }
