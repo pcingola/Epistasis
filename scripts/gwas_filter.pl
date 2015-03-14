bf7faace335040efb47efe1045cb9271bae88ca9
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-03-14 14:54:52 -0400

    Project updated

---
+++ scripts/gwas_filter.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+$absTh = 3.0;
+
+while( $l =<STDIN> ) {
+	chomp $l;
+	@t = split /\t/, $l;
+
+	# Parse log(Bf)
+	($logBf, $logBfLogReg) = ( $t[3], $t[5] );
+
+	# Show only if BF is increased by LL(MSA)
+	$deltaLogBf = $logBf - $logBfLogReg;
+
+	if( $deltaLogBf > 0 ) {
+		# Parse logistic regression coefficients
+		$thetaAlt = $t[19];
+		$thetaAlt =~ tr/\[\]//d;
+		@ths = split /,/, $thetaAlt;
+		$sumAbs = abs($ths[0]) + abs($ths[1]);
+
+		if( ( $absTh * $sumAbs ) < abs($ths[2]) ) {
+			print "$deltaLogBf\t$ths[0]\t$ths[1]\t$ths[2]\t$l\n";
+		}
+	}
+}
