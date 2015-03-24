5c0df50260ec6aa7a94fcd5d7052ed71dda9e005
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-03-24 10:06:59 -0400

    Project updated

--- r/logisticRegressionPowerGT.r
+++ r/logisticRegressionPowerGT.r
@@ -65,84 +65,46 @@ testLr <- function(perc, n, af1, af2, beta) {
 	cat(perc * 100, '%\tn:', n, '\taf1:', af1, '\taf2:', af2, '\taf12:', af12, '\tbeta3:', beta3,'\tp-value (LR):', pvalueLr, '\tbeta.null:', lr0$coefficients, '\tbeta.alt:', lr1$coefficients, '\tprevalecense:', (sum(y)/length(y)), '\n')
 }
 
-#  #-------------------------------------------------------------------------------
-#  # Perform Logistic regression test
-#  #-------------------------------------------------------------------------------
-#  
-#  testLr <- function(n, beta, af) {
-#  	# Create input
-#  	var1 <- randGt(n, af)
-#  	var2 <- randGt(n, af)
-#  	var12 <- abs( var1 - var2 )
-#  	ones <- rep(1, n)
-#  	X <- cbind( ones, var1, var2, var12 )	# As a matrix
-#  
-#  	# Logit(pi)
-#  	logitp = X %*% beta
-#  	p = 1 / ( 1 + exp(-logitp) )
-#  
-#  	# P(yi | X)
-#  	r <- runif(n)
-#  	y <- rep(0, n)
-#  	y[ r <= p ] = 1
-#  
-#  	# Logistic regression
-#  	lr0 <- glm( y ~ var1 + var2 + var12 , family=binomial) 
-#  	lr1 <- glm( y ~ var1 + var2         , family=binomial) 
-#  
-#  	# Likelyhood ratio test
-#  	lrt <- lrtest(lr0, lr1)
-#  	pvalueLr <- lrt$p.value   # p-value from likelihood ratio test
-#  	lrSum <- summary( lr0 )
-#  	pvalueWald <- lrSum$coefficients[4,4]
-#  	if( debug )	cat('\t\tp-value (LR):', pvalueLr, '\tp-value (Wald):', pvalueWald, '\n')
-#  
-#  	return( c(pvalueLr, pvalueWald) )
-#  }
-
 #-------------------------------------------------------------------------------
 # Main
 #-------------------------------------------------------------------------------
 
-if( F ) {
-	# for( n in seq(1000, 10000, 100) ) {
-	for( n in seq(1000, 1000, 100) ) {
-		pvals <- matrix( rep(0, 2*m), ncol=2 )
-		for( i in 1:m ) {
-			pvals[i,] <- testLr(n, beta, af)
-		}	
-		cat('Iterations:', m, '\tSize:', n, '\tMean p-value (LR):', mean(pvals[,1]), '\tp-value (Wald):', mean(pvals[,2]), '\n')
-	}
-}
+#---
+# Parse command line arguments
+#---
+
+if( !exists('cmdLineArgs') ) cmdLineArgs <- commandArgs(trailingOnly = TRUE);
+
+# Stop if there are no command line argument
+if( length(cmdLineArgs) < 1 ) { fatalError('No command line arguments!\n'); }
+
+iter  <- as.integer(cmdLineArgs[1]);	# Number of tests
+n     <- as.integer(cmdLineArgs[2]);	# Number of cases and controls
+af1   <- as.numeric(cmdLineArgs[3]);	# Allele frequency for variant 1
+af2   <- as.numeric(cmdLineArgs[4]);	# Allele frequency for variant 2
+beta3 <- as.numeric(cmdLineArgs[5]);	# Interaction term
+prev  <- as.numeric(cmdLineArgs[6]);	# Disease prevalecense: e.g. 8% for type II diabetes
+
+cat('Parameters\n')
+cat('\titer  :', iter, '\n')
+cat('\tn     :', n, '\n')
+cat('\taf1   :', af1, '\n')
+cat('\taf2   :', af2, '\n')
+cat('\tbeta3 :', beta3, '\n')
+cat('\tprev  :', prev, '\n')
+cat('\tprev  :', (1-af1), '\n')
 
 #---
 # Initial parameters
 #---
-iter <-	100		# Number of tests
-n <- 1000		# Number of cases and controls
-
-af1 = 0.1		# Allele frequency for variant 1
-af2 = 0.1		# Allele frequency for variant 2
-
-prev = 0.08		# Disease prevalecense 8% (type II diabetes)
 beta0 = log(prev/(1-prev))
-
-# Other parameters
-beta1 = 0
+beta1 = 0		# Other parameters are assumed to be zero
 beta2 = 0
-beta3 = 2		# Interaction parameter
 
-for( beta3 in c(0.1, 0.5, 1, 2, 5) ) {
-	for( n in c(1000000, 100000, 50000, 25000, 10000, 1000) ) {
-		for( af1 in seq(0.01,0.1,0.01) ) {
-			for( af2 in seq(0.01,0.1,0.01) ) {
-				beta <- c(beta0, beta1, beta2, beta3)
-
-				for( i in 1:iter ) {
-					testLr(i/iter, n, af1, af2, beta)
-				}
-			}
-		}
-	}
+#---
+# Test logistic regression 'iter' times
+#---
+for( i in 1:iter ) {
+	testLr(i/iter, n, af1, af2, beta)
 }
 
